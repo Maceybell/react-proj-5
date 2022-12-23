204cfcc8f497374fd5d1d341007986d75255f7d4
Macey Bell <maceytbell@gmail.com>
2022-12-23 13:19:18 -0700

    finished country app

---
+++ src/Components/Weather.js
@@ -0,0 +1,64 @@
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { selectDisplay } from "../redux/slices/displayCountrySlice";
+import React, { useEffect, useState } from "react";
+
+const Weather = () => {
+  const [weather, setWeather] = useState();
+  let display = useSelector(selectDisplay);
+  let latitude = display.capitalInfo.latlng[0];
+  let longitude = display.capitalInfo.latlng[1];
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      url: "https://weatherapi-com.p.rapidapi.com/current.json",
+      params: { q: `${latitude}, ${longitude}` },
+      headers: {
+        "X-RapidAPI-Key": "9d7e5ddaa5msh65f1de5724b480dp1b9979jsn88ee370c6e73",
+        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+      },
+    };
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        setWeather(response.data)
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  });
+
+  return (
+    <div>
+      <table className="overview-table">
+        <tr>
+          <td>Conditions: </td>
+          <td>{weather?.current?.condition?.text}</td>
+        </tr>
+        <tr>
+          <td>Temperature: </td>
+          <td>{weather?.current?.temp_f} degrees Fahrenheit</td>
+        </tr>
+        <tr>
+          <td>Feels Like: </td>
+          <td>{weather?.current?.feelslike_f} degrees Fahrenheit</td>
+        </tr>
+        <tr>
+          <td>Humidity: </td>
+          <td>{weather?.current?.humidity}%</td>
+        </tr>
+        <tr>
+          <td>Wind Speed: </td>
+          <td>
+            {weather?.current?.wind_mph} mph {""}
+            {weather?.current?.wind_dir}
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
+};
+
+export default Weather;
